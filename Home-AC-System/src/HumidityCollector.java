1a817d8721b06b5c32824634bac34f884282a729
abhinav-rafa <schaud70@asu.edu>
2023-04-20 22:09:05 -0700

    Added logic for temp and humidity controller

--- Home-AC-System/src/HumidityCollector.java
+++ Home-AC-System/src/HumidityCollector.java
@@ -1,6 +1,41 @@
+import java.util.Collections;
+import java.util.List;
+
 /**
  * HumidityCollector - Middleman between humidity sensor and backend, provides average humidity
  */
 
 public class HumidityCollector {
+    // state variables
+    private int sum;
+    private int round;
+
+    //output
+    private int avHumidity;
+
+    /**
+     * Class constructor
+     */
+    public HumidityCollector(){
+        this.sum = 0;
+        this.round = 0;
+    }
+
+    /**
+     * Function to get the current room humidity and calculate the average humidity over 30 rounds or 30 secs
+     * @return   The avHumidity at every 30th round and -1 otherwise
+     */
+    public int sendHumidityValue(int temp) {
+        this.round += 1;
+        this.sum += temp;
+
+        if (this.round == 30) {
+            this.avHumidity = this.sum/this.round;
+            this.round = 0;
+
+            return this.avHumidity;
+        }
+
+        return -1;
+    }
 }
--- Home-AC-System/src/TempCollector.java
+++ Home-AC-System/src/TempCollector.java
@@ -1,6 +1,44 @@
+import java.util.*;
+
 /**
  * TempController - Middleman between temperature sensor and backend, provides average temperature
  */
 
 public class TempCollector {
+
+    // state variables
+    private int sum;
+    private int round;
+    private List<Integer> temperatures;
+
+    //output
+    private int avTemp;
+
+    /**
+     * Class constructor
+     */
+    public TempCollector(){
+        this.sum = 0;
+        this.round = 0;
+        temperatures = Collections.emptyList();
+    }
+
+    /**
+     * Function to get the current room temperature and calculate the average temperature over 30 rounds or 30 secs
+     * @return   The avTemp at every 30th round and -1 otherwise
+     */
+    public int sendTempValue(int temp) {
+        this.round += 1;
+        this.sum += temp;
+
+        if (this.round == 30) {
+            this.avTemp = this.sum/this.round;
+            this.round = 0;
+
+            return this.avTemp;
+        }
+
+        return -1;
+    }
+
 }
