a10916a1b4ffe4c5b52a3054c38474051e1acf0f
adesai41 <112652525+adesai41@users.noreply.github.com>
2023-04-24 18:31:45 -0700

    Reading mode and tempreture

--- Home-AC-System/src/ThermostatThread.java
+++ Home-AC-System/src/ThermostatThread.java
@@ -25,7 +25,6 @@ public class ThermostatThread extends Thread {
 
 
         try {
-            int[] data = new int[2];
             String file = "/Users/aakankshadesai/Documents/GitHub/automatic-home-ac-system/Home-AC-System/Data/inputOne.txt";//file path
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
@@ -33,8 +32,9 @@ public class ThermostatThread extends Thread {
             String line = br.readLine();
             while(line != null)
             {
-                //int mode = line.split(",");//splitting lines
-                System.out.println("*********** mode ***************" + data[0]);
+                String[] data = line.split(", ");
+                thermostat.setMode(Integer.parseInt(data[0]));
+                thermostat.setSetTemp(Integer.parseInt(data[0]));
                 line = br.readLine();
             }
             br.close();
@@ -42,26 +42,6 @@ public class ThermostatThread extends Thread {
         {
             System.out.print(e);
         }
-
-//        try {
-//            for (int i=1; i<=3; i++) {
-//
-//
-////                if (i==1) {
-////                    thermostat.setSetTemp(autoSetting1[1]);
-////                } else if (i==2) {
-////                    thermostat.setSetTemp(autoSetting2[1]);
-////                }
-//////                if (i==3) {
-//////                    thermostat.setSetTemp(heatSetting3[1]);
-//////                }
-//
-//                Thread.sleep(1000 * 30);
-//
-//            }
-//        } catch (InterruptedException e) {
-//            System.out.println(e);
-//        }
 
     }
 
