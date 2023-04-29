65c27cc1dd2c79c9c2f9adde2de3ccdc672f8b84
srishti-chaudhary <schaud70@asu.edu>
2023-04-28 17:29:31 -0700

    added comments for testMode

--- Home-AC-System/src/Main.java
+++ Home-AC-System/src/Main.java
@@ -4,13 +4,14 @@
 
 public class Main {
     public static void main(String[] args){
-        int mode = 1;
-        int temp = 24;
 
-        temp = mode == 1 ? 30 : mode == 2 ? 10 : 24;
+        // Change the mode to the mode you want to test it for, this will allow the simulator to generate lower temperature for heat mode
+        // and higher temperature for cool mode ( COOL = 1, HEAT = 2, AUTO = 3)
+        int testMode = 1;
+        int initialTemp = testMode == 1 ? 30 : testMode == 2 ? 10 : 24;
 
         //create instance of main component TempController
-        TempController tempController = new TempController(temp);
+        TempController tempController = new TempController(initialTemp);
 
         //create instances of all controller class
         FanController fanController = new FanController();
@@ -21,9 +22,9 @@ public class Main {
         HumidityCollector humidityCollector = new HumidityCollector();
         TempCollector tempCollector = new TempCollector(tempController);
 
-        Thermostat thermostat = new Thermostat(temp, modeController, tempInputController, fanController);
+        Thermostat thermostat = new Thermostat(initialTemp, modeController, tempInputController, fanController);
 
-        EnviroSim simulator = new EnviroSim(temp, temp, tempCollector, tempController, modeController, tempInputController, humidityCollector, thermostat, fanController);
+        EnviroSim simulator = new EnviroSim(initialTemp, initialTemp, tempCollector, tempController, modeController, tempInputController, humidityCollector, thermostat, fanController);
 
         // Start the simulation
         simulator.environmentSim(5);
